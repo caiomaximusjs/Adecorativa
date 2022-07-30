0640c8c259226e5a978f0a40a8ca660265ea4c0c
caiomaximusjs <caiomaximus@hotmail.com>
2022-07-30 20:30:58 -0300

    FIX css class renamed

--- adecorativa/adecorativa/src/components/Sobre.js
+++ adecorativa/adecorativa/src/components/Sobre.js
@@ -6,11 +6,11 @@ function Sobre() {
   return (
 
     <div>
-      <h1 className={styles.titulo} >Conheça-nos um pouco mais</h1>
+      <h1 className={styles.sobre_titulo} >Conheça-nos um pouco mais</h1>
 
-      <section className={styles.container}>
+      <section className={styles.sobre_container}>
 
-        <div id='sobrenos'className={styles.sobrenos}>
+        <div id='sobrenos'className={styles.sobre_container_texto}>
           <p>Com rapidez e qualidade A Decorativa presta atendimento personalizado e exclusivo. Através de profissionais experientes tudo é realizado para melhor atendê-lo no momento difícil da perda de um ente querido. Os nossos colaboradores são capacitados para fornecer assistência, prestar assessoramento e oferecer total apoio às famílias em luto, desempenhando seu trabalho com dedicação, responsabilidade e serenidade.  </p>
 
         </div>
