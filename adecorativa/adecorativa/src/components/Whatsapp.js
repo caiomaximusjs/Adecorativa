e5134780248a620fa90f29109961a2fc4006188a
caiomaximusjs <caiomaximus@hotmail.com>
2022-08-04 01:35:20 -0300

    UPDATE project ready for deploy

--- adecorativa/adecorativa/src/components/Whatsapp.js
+++ adecorativa/adecorativa/src/components/Whatsapp.js
@@ -3,7 +3,7 @@ import styles from './Whatsapp.module.css'
 function Whatsapp () {
   return(
     <div>
-      <a className={styles.telefone_ligar}href='https://www.google.com/search?q=a+decorativa+jardim+da+saudade&oq=a+decorativa+jardim&aqs=chrome.0.69i59j69i57j0i15i22i30j69i59j69i60l3.3272j0j4&sourceid=chrome&ie=UTF-8'><i class="bi bi-telephone-outbound-fill"></i></a>
+      
       <a href='https://api.whatsapp.com/send?phone=5571996098565&text=Ol%C3%A1%2C%20Poderia%20me%20ajudar%3F' target='blank'>
        <img className={styles.zap} src={zap}></img>
        </a>
